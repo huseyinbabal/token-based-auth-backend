38edaf293bc6ff35c8c5da99f123ba43d6012b49
Hüseyin BABAL <huseyinbabal88@gmail.com>
2014-09-19 23:49:27 +0300

    cors added

--- server.js
+++ server.js
@@ -60,10 +60,13 @@ app.post('/signin', function(req, res) {
                 userModel.email = req.body.email;
                 userModel.password = req.body.password;
                 userModel.save(function(err, user) {
-                    res.json({
-                        type: true,
-                        data: user,
-                        token: jwt.sign(user, process.env.JWT_SECRET)
+                    user.token = jwt.sign(user, process.env.JWT_SECRET);
+                    user.save(function(err, user1) {
+                        res.json({
+                            type: true,
+                            data: user1,
+                            token: user1.token
+                        });
                     });
                 })
             }
