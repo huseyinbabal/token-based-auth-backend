7e9033dcfd0a14080150bd4f392a33cd4373a36d
Hüseyin BABAL <huseyinbabal88@gmail.com>
2014-09-19 23:09:35 +0300

    user ops added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express    = require("express");
 var morgan     = require("morgan");
 var bodyParser = require("body-parser");
-var expressJwt = require("express-jwt");
+var jwt        = require("jsonwebtoken");
 var mongoose   = require("mongoose");
 var app        = express();
 
@@ -19,11 +19,72 @@ app.use(morgan("dev"));
 
 
 app.post('/authenticate', function(req, res) {
+    User.findOne({email: req.body.email, password: req.body.password}, function(err, user) {
+        if (err) {
+            res.json({
+                type: false,
+                data: "Error occured: " + err
+            });
+        } else {
+            res.json({
+                type: true,
+                data: user,
+                token: user.token
+            });
+        }
+    });
+});
 
+
+app.post('/signin', function(req, res) {
+    User.findOne({email: req.body.email, password: req.body.password}, function(err, user) {
+        if (err) {
+            res.json({
+                type: false,
+                data: "Error occured: " + err
+            });
+        } else {
+            if (user) {
+                res.json({
+                    type: false,
+                    data: "User already exists!"
+                });
+            } else {
+                var userModel = new User();
+                userModel.email = req.body.email;
+                userModel.password = req.body.password;
+                userModel.save(function(err, user) {
+                    res.json({
+                        type: true,
+                        data: user,
+                        token: jwt.sign(user, process.env.JWT_SECRET)
+                    });
+                })
+            }
+        }
+    });
 });
 
 app.get('/me', function(req, res) {
-
+    var bearerToken;
+    var bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== 'undefined') {
+        var bearer = bearerHeader.split(" ");
+        bearerToken = bearer[1];
+    }
+    User.findOne({token: bearerToken}, function(err, user) {
+        if (err) {
+            res.json({
+                type: false,
+                data: "Error occured: " + err
+            });
+        } else {
+            res.json({
+                type: true,
+                data: user
+            });
+        }
+    });
 });
 
 process.on('uncaughtException', function(err) {
